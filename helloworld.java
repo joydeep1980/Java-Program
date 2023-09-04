2ad0903c5c1eb3e7bf2f1c29479d3e551e2f4183
joydeep1980 <48094852+joydeep1980@users.noreply.github.com>
2023-09-04 17:43:04 +0530

    Update helloworld.java fpr poll scm

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello This is joydeep");  
      System.out.println("Sample java program");
+    System.out.println("Added poll SCM in jenkins");
     }  
 }
